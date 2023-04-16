42e1f6e94d5eb956005abfcfa43f1b8a980c69fd
mohammed afzal <afzalmd03@gmail.com>
2023-04-16 18:12:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    const first = () => {
+      var requestOptions = {
+        method: "GET",
+        redirect: "follow",
+      };
+
+      fetch("http://localhost:4000/Books", requestOptions)
+        .then((response) => response.json())
+        .then((result) => setNews(result))
+        .catch((error) => console.log("error", error));
+    };
+    first();
+  }, []);
   return (
     <div className="App">
       <h1>React JS CRUD Operations</h1>
+      {news.map((newsItems) => (
+        <h1>{newsItems.Name}</h1>
+      ))}
     </div>
   );
 }
